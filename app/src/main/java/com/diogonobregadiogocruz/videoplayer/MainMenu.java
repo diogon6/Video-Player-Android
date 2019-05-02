c078284d24dfd0b2fc020640dbda72e24d8f6bd1
Diogo Nobrega <diogonobrega6@gmail.com>
2019-05-02 15:47:28 +0100

    stuff

--- app/src/main/java/com/diogonobregadiogocruz/videoplayer/MainMenu.java
+++ app/src/main/java/com/diogonobregadiogocruz/videoplayer/MainMenu.java
@@ -1,14 +1,24 @@
 package com.diogonobregadiogocruz.videoplayer;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.EditText;
 import android.widget.RadioButton;
 
 public class MainMenu extends AppCompatActivity {
 
+    EditText fileName;
     RadioButton rbfile, rburl;
+    private static final int READ_REQUEST_CODE = 42;
+    private String filemanagerstring, selectedImagePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +28,7 @@ public class MainMenu extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        fileName = findViewById(R.id.fileEditText);
         rbfile = findViewById(R.id.fileRadioButton);
         rburl = findViewById(R.id.urlRadioButton);
     }
@@ -39,6 +50,30 @@ public class MainMenu extends AppCompatActivity {
         {
             rbfile.setChecked(false);
             rburl.setChecked(true);
+        }
+    }
+
+    public void chooseVideo (View view)
+    {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+        intent.setType("video/*");
+
+        startActivityForResult(intent, READ_REQUEST_CODE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
+
+        if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            Uri uri = null;
+            if (resultData != null) {
+                uri = resultData.getData();
+
+                fileName.setText(uri.toString());
+            }
         }
     }
 }
