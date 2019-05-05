c6f248c93fded7fe858cf2e9c109d3dd2f1aa662
diogon6 <32933699+diogon6@users.noreply.github.com>
2019-05-05 17:36:09 +0100

    Horizontal videos can now rotate and fullscreen working

--- app/src/main/java/com/diogonobregadiogocruz/videoplayer/VideoMenu.java
+++ app/src/main/java/com/diogonobregadiogocruz/videoplayer/VideoMenu.java
@@ -1,15 +1,29 @@
 package com.diogonobregadiogocruz.videoplayer;
 
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.graphics.drawable.Drawable;
+import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Build;
+import android.os.CountDownTimer;
+import android.support.annotation.DrawableRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewTreeObserver;
 import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.MediaController;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 public class VideoMenu extends AppCompatActivity {
 
     VideoView video;
+    ImageView fullscreenButton;
+    private static Boolean fullscreen;
+    private CountDownTimer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +33,9 @@ public class VideoMenu extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        fullscreen = false;
         video = findViewById(R.id.videoView);
+        fullscreenButton = findViewById(R.id.fullscreenIcon);
         Uri uri;
 
         Bundle extras = getIntent().getExtras();
@@ -35,5 +51,63 @@ public class VideoMenu extends AppCompatActivity {
         MediaController mediaController = new MediaController(this);
         video.setMediaController(mediaController);
         mediaController.setAnchorView(video);
+
+
+        //Determine if the video is horizontal or vertical
+        //If it is vertical, it locks the screen rotation to PORTRAIT
+        video.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(final MediaPlayer mp) {
+
+                if(mp.getVideoHeight() > mp.getVideoWidth())
+                {
+                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+                    fullscreenButton.setVisibility(View.GONE);
+                }
+            }
+        });
+    }
+
+    public void fullScreenToggle(View view)
+    {
+        if(!fullscreen)
+        {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            fullscreenButton.setBackground(getDrawable(R.drawable.fullscreen_exit_icon));
+            fullscreen = true;
+        }
+        else
+        {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            fullscreenButton.setBackground(getDrawable(R.drawable.fullscreen_icon));
+            fullscreen = false;
+        }
+
+        // After 5 seconds the screen rotation is unlocked
+        timer = new CountDownTimer(5000, 1000) {
+
+            public void onTick(long millisUntilFinished) {}
+
+            public void onFinish() {
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+            }
+        }.start();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        // Checks the orientation of the screen
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE)
+        {
+            fullscreenButton.setBackground(getDrawable(R.drawable.fullscreen_exit_icon));
+            fullscreen = true;
+        }
+        if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT)
+        {
+            fullscreenButton.setBackground(getDrawable(R.drawable.fullscreen_icon));
+            fullscreen = false;
+        }
     }
 }
